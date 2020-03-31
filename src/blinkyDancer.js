4999434a37b2e59759da959cc22ffe18ca086083
Robert Heler <robertheler@gmail.com>
2020-03-30 19:03:58 -0700

    Convert to pseudoclassical inheritance

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,6 +1,5 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
-
   this.timeBetweenSteps = timeBetweenSteps;
   this.top = top;
   this.left = left;
@@ -16,8 +15,7 @@ makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
 makeBlinkyDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
-  console.log(this);
-  this.oldStep();
+  makeDancer.prototype.step.call(this);
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
--- src/dancer.js
+++ src/dancer.js
@@ -1,28 +1,21 @@
 // Creates and returns a new makeDancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
-
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="makeDancer"></span>');
   this.timeBetweenSteps = timeBetweenSteps;
-  this.top = top;
-  this.left = left;
-  //console.log(this);
   // now that we have defined the makeDancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.step();
   this.setPosition();
-
 };
 
 makeDancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
-
   var styleSettings = {
     top: top,
     left: left
   };
-
   this.$node.css(styleSettings);
 };
 
@@ -31,3 +24,4 @@ makeDancer.prototype.step = function() {
   // it just schedules the next step
   setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
+
